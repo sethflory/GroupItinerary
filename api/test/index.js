3be382d35330aef72d86fce2a34c0dbe8ef8c57a
Seth Flory <sethflory@gmail.com>
2026-01-28 20:37:29 -0500

    Strip to bare minimum - only hello function, no deps

--- api/test/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: { message: "Test function works!", timestamp: new Date().toISOString() }
-  };
-};
